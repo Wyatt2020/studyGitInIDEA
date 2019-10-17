89e9dee8c8b79dd37271c9f0e19a6e8beae8d706
Wyatt2020 <hyw2020@foxmail.com>
2019-10-17 14:12:52 +0800

    message

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -2,6 +2,5 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("Git is a distributed version control system.");
         System.out.println("新建了dev分支，添加了新的功能");
-        System.out.println("合并分支");
     }
 }
